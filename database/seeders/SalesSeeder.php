5a4a6ee12152758bdcf74586679e28509b90e77b
iyumoyumo <71491764+iyumoyumo@users.noreply.github.com>
2026-06-20 12:41:20 +0900

    Update SalesSeeder.php

--- database/seeders/SalesSeeder.php
+++ database/seeders/SalesSeeder.php
@@ -37,6 +37,127 @@ class SalesSeeder extends Seeder
                 'sale_date' => '2024-07-02',
                 'memo' => '紹介案件'
             ],
+   [
+                'employee_id' => '1001',
+                'amount' => 2000,
+                'sale_date' => '2024-08-01',
+                'memo' => '初回売上'
+            ],
+            [
+                'employee_id' => '1002',
+                'amount' => 1000,
+                'sale_date' => '2024-08-02',
+                'memo' => '紹介案件'
+            ],
+[
+                'employee_id' => '1001',
+                'amount' => 10000,
+                'sale_date' => '2024-09-01',
+                'memo' => '初回売上'
+            ],
+            [
+                'employee_id' => '1002',
+                'amount' => 10000,
+                'sale_date' => '2024-09-02',
+                'memo' => '紹介案件'
+            ],
+   [
+                'employee_id' => '1001',
+                'amount' => 5000,
+                'sale_date' => '2024-10-10',
+                'memo' => '初回売上'
+            ],
+            [
+                'employee_id' => '1002',
+                'amount' => 8000,
+                'sale_date' => '2024-10-12',
+                'memo' => '紹介案件'
+            ],
+[
+                'employee_id' => '1001',
+                'amount' => 10000,
+                'sale_date' => '2024-11-03',
+                'memo' => '初回売上'
+            ],
+            [
+                'employee_id' => '1002',
+                'amount' => 10000,
+                'sale_date' => '2024-11-04',
+                'memo' => '紹介案件'
+            ],
+   [
+                'employee_id' => '1001',
+                'amount' => 5000,
+                'sale_date' => '2024-12-21',
+                'memo' => '初回売上'
+            ],
+            [
+                'employee_id' => '1002',
+                'amount' => 8000,
+                'sale_date' => '2024-12-22',
+                'memo' => '紹介案件'
+            ],
+[
+                'employee_id' => '1001',
+                'amount' => 10000,
+                'sale_date' => '2025-05-01',
+                'memo' => '初回売上'
+            ],
+            [
+                'employee_id' => '1002',
+                'amount' => 10000,
+                'sale_date' => '2025-05-02',
+                'memo' => '紹介案件'
+            ],
+   [
+                'employee_id' => '1001',
+                'amount' => 5000,
+                'sale_date' => '2025-06-03',
+                'memo' => '初回売上'
+            ],
+            [
+                'employee_id' => '1002',
+                'amount' => 8000,
+                'sale_date' => '2025-06-24',
+                'memo' => '紹介案件'
+            ],
+[
+                'employee_id' => '1001',
+                'amount' => 10000,
+                'sale_date' => '2025-07-26',
+                'memo' => '初回売上'
+            ],
+            [
+                'employee_id' => '1002',
+                'amount' => 10000,
+                'sale_date' => '2025-07-22',
+                'memo' => '紹介案件'
+            ],
+
+   [
+                'employee_id' => '1001',
+                'amount' => 5000,
+                'sale_date' => '2025-08-01',
+                'memo' => '初回売上'
+            ],
+            [
+                'employee_id' => '1002',
+                'amount' => 8000,
+                'sale_date' => '2025-08-02',
+                'memo' => '紹介案件'
+            ],
+[
+                'employee_id' => '1001',
+                'amount' => 10000,
+                'sale_date' => '2025-09-01',
+                'memo' => '初回売上'
+            ],
+            [
+                'employee_id' => '1002',
+                'amount' => 10000,
+                'sale_date' => '2025-09-02',
+                'memo' => '紹介案件'
+            ],
 
         ]);
     }
